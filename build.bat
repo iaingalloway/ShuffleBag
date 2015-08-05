7004ef182783c37fd1338849b3f0be7e2809b98c
Iain Galloway <iain.galloway@gmail.com>
2015-08-05 14:02:17 +0100

    Added package restore to build.bat

--- build.bat
+++ build.bat
@@ -14,10 +14,11 @@ if "%nuget%" == "" (
 	set nuget=nuget
 )
 
+%nuget% restore src\ShuffleBag.sln
+
 %WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild src\ShuffleBag.sln /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=diag /nr:false
 
 mkdir Build
 mkdir Build\lib
-mkdir Build\lib\net40
 
 %nuget% pack "src\ShuffleBag.nuspec" -NoPackageAnalysis -verbosity detailed -o Build -Version %version% -p Configuration="%config%"
