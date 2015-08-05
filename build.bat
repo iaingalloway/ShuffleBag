012e93469faa7766601b328b265815007d83c09a
Iain Galloway <iain.galloway@gmail.com>
2015-08-05 14:19:34 +0100

    Updated build.bat

--- build.bat
+++ build.bat
@@ -18,4 +18,7 @@ if "%nuget%" == "" (
 
 %WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild src\ShuffleBag.sln /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=diag /nr:false
 
+mkdir Build
+mkdir Build\lib
+
 %nuget% pack "src\ShuffleBag.nuspec" -NoPackageAnalysis -verbosity detailed -o Build -Version %version% -p Configuration="%config%"
